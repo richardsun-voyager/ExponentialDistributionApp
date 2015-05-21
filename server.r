ceb459d0032da3f26ed0a32de462c79cc397c1bd
topsun888 <topsun888@yahoo.com>
2015-05-21 13:06:07 +0800

    initial commit

--- server.r
+++ server.r
@@ -1,16 +1,17 @@
-library(ggplot2)
+cfunc <- function(x,lambda,n) sqrt(n) * (mean(x) - 1/lambda) / 1/lambda
+
 shinyServer(
 function(input, output) {
 output$figure<-renderPlot({
-lambda<-input$lambda
-n<-input$popSize
-scale<-input$repTimes
+lambda<-input$lambda ## set lambda value
+n<-input$popSize ##set population size
+scale<-input$repTimes ##set repetition times
 set.seed(111)
-expArray<-matrix(rexp(n*scale,lambda),scale)
-meanArray<-apply(expArray,1,mean)
+expArray<-matrix(rexp(n*scale,lambda),scale) ##create a matrix
+meanArray<-apply(expArray,1,cfunc,lambda,n) ## calculate the mean value of each group
 hist(meanArray,breaks=20,freq=FALSE,xlab="Mean of The Samples",main="Histogram Of Averages of Exponential Distribution")
 if(input$meanLine)
-abline(v=1/lambda,col="red",lwd=3)
+abline(v=0,col="red",lwd=3)
 }
 
 
--- ui.r
+++ ui.r
@@ -14,9 +14,10 @@ shinyUI(navbarPage("Exponential Distribution Simulation Navbar!",
     )
   )
 ),  
-   tabPanel("Supporting Document",
-   p("This application aims to do simulations on exponential distribution and Central Limit Theorem. Users can set the values of lambda,population size and repetition times of exponential simulation. Then the averages of each population are calculated and demonstrated in a histogram, users can choose to add the mean line or not in order to make comparisons between theoretical mean and simulation mean.Furthermore, CLT can also be testified in this histogram.")
-   )
+   tabPanel("Supporting Document",  
+   h3("This application aims to do simulations on exponential distribution and Central Limit Theorem."),
+p("1.Users can set the values of lambda,population size(step by 10) and repetition times of exponential simulation(step by 100). The generalized averages of each population are calculated and demonstrated in a histogram."),
+p("2.Users can choose to add the mean line or not in order to make comparisons between theoretical mean and simulation result.") )
   
 )
 )
